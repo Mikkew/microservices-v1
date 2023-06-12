1e5ed478f8b7763477ac0f03e515edd6c6c9832c
Miguel Medina <miguelmedinasec.94@gmail.com>
2023-06-12 00:34:18 -0600

    Spring Cloud Stream

---
+++ mms-cloudstream-example/src/test/java/com/mms/cloudstream/MmsCloudstreamExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms.cloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsCloudstreamExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
